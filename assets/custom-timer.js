d7a9098e3a54cde0374d035ce2fad58b27cca33c
chandu-kuruba <chandu.k@marmeto.com>
2024-02-27 11:10:42 +0530

    Added styles to pagination

--- assets/custom-timer.js
+++ assets/custom-timer.js
@@ -3,14 +3,13 @@
 function updateCountdown(endTime) {
   const now = new Date();
   const timeDifference = endTime - now;
-  const remainingTime=new Date(timeDifference)
-  const days = remainingTime.getDate()
-  const hours = remainingTime.getHours()
-  const minutes = remainingTime.getMinutes()
-  const seconds = remainingTime.getSeconds()
 
+  const millisecondsInADay = 1000 * 60 * 60 * 24;
+  const days = Math.floor(timeDifference / millisecondsInADay);
+  const hours = Math.floor((timeDifference % millisecondsInADay) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-  // Update the countdown elements
   // Update the countdown elements
   const daysValue = padZero(days);
   document.getElementById('days').textContent = daysValue.firstDigit.toString() + daysValue.secondDigit.toString();
@@ -47,6 +46,8 @@ function padZero(number) {
 // Set the end time for the countdown (replace with your desired end time)
 const enteredDate = document.querySelector("#date").dataset.date
 const endTime = new Date(`${ enteredDate }`);
-
+console.log(endTime)
+console.log(new Date())
+console.log(new Date(endTime - new Date()))
 // Start the countdown
 updateCountdown(endTime);
